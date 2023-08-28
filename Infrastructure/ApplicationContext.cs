7dff56dbcf23abb3995ac4b21e020bdf113d9ace
MAAghakabiri <a.aghakabiri@gmail.com>
2023-08-28 22:54:45 +0330

    #39 اضافه کردن موجودیت کاربر اقدامات: افزودن موجودیت کاربر در لایه دامین و افزودن فیلد های مربوطه

---
+++ Infrastructure/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class ApplicationContext: DbContext
+    {
+        //public ApplicationContext(DbContextOptions<ApplicationDbContext> options)
+        //               : base(options)
+        //{
+        //}
+        //#region Tables
+        //public virtual DbSet<Business> Businesses { get; set; }
+        //#endregion
+    }
+}
