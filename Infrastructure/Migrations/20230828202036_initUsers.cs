01e231b844be3267ce54a6b755729f5a00e9d0b3
MAAghakabiri <a.aghakabiri@gmail.com>
2023-08-28 23:57:35 +0330

    #39 افزودن مایگریشن کاربران اقدامات: ساخت مایگریشن کاربران !اپدیت دیتابیس اصلاح کانکشن استرینگ دیتابیس

---
+++ Infrastructure/Migrations/20230828202036_initUsers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mobile = table.Column<long>(type: "bigint", nullable: false),
+                    VerificationCode = table.Column<int>(type: "int", nullable: false),
+                    ExpireVerificationCode = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RegisterDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
